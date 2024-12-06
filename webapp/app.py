8a0583d6754ba25cd5deb9400441f444af877d34
row-huh <rohaabropathan@gmail.com>
2024-12-06 16:54:33 +0500

    created landing page and configured routes from landing page to image uploading

--- webapp/app.py
+++ webapp/app.py
@@ -1,17 +1,32 @@
 from flask import Flask, render_template
-from model.model import AI # model/model.py - class AI 
-
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    model = AI()
+    # TODO
+    # configure landing page
+    # user should press a continue button then get redirected to a different route
     
-    # here is the response variable which needs to be displayed in the chat interface
-    response = model.get_response("hi")
     
+    return render_template('index.html')
 
+
+@app.route('/image')
+def image():
+    #TODO
+    # upload image
+    # send to ml, get back a response and display it
+    # something like "This image shows xyz disease"
+    # show another button named "Proceed to chat" (or something else)
+    # upon clicking that, should be directed to /chat and have the llm enter the chat then
+    return "This is image route"
+
+@app.route('/chat')
+def chat():
+    #TODO
+    # get first message that should be the diagnosis
+    ...
 
 if __name__ == '__main__':
     app.run(debug=True)
